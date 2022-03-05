b381a04e6823f2b860bc3b40cb3d620db84a7255
Artem Nikolaiev <artyom.nikolaev@yahoo.com>
2022-03-05 22:01:26 +0200

    feat(gof-dp): add interpreter pattern reference for c#

--- csharp/pattenrs/behavioral/Interpreter.cs
+++ csharp/pattenrs/behavioral/Interpreter.cs
@@ -14,14 +14,110 @@ namespace Interpreter
         public static void Run()
         {
             Console.WriteLine("\nInterpreter");
+
+            // init context
+            var ctx = new Context();
+
+            // set context variables
+            ctx.SetVariable("x", 2);
+            ctx.SetVariable("y", 4);
+            ctx.SetVariable("z", 8);
+
+            // init expression
+            var expression = new SubstractExpression(
+                new AddExpression(
+                    new NumberExpression("y"), // 4 + 8 = 12
+                    new NumberExpression("z")
+                ),
+                new NumberExpression("x") // 12 - 2 = 10
+            );
+
+            // interpret expression
+            var result = expression.Interpret(ctx);
+
+            Console.WriteLine($"Expression result: {result}");
+        }
+    }
+
+    // -- Context
+
+    /// <summary>Class <c>Context</c> represents interpretation context.</summary>
+    public class Context
+    {
+        private Dictionary<string, int> variables;
+        public Context()
+        {
+            variables = new Dictionary<string, int>();
+        }
+
+        public void SetVariable(string name, int value)
+        {
+            variables[name] = value;
+        }
+
+        public int GetVariable(string name)
+        {
+            if (variables.ContainsKey(name))
+            {
+                return variables[name];
+            }
+            return 0;
         }
     }
 
     // -- Abstract Expression
 
+    /// <summary>Interface <c>IExpression</c> describes expression.</summary>
+    public interface IExpression
+    {
+        public int Interpret(Context ctx);
+    }
+
     // -- Concreate Expression
 
-    // -- Context
-}
+    /// <summary>Class <c>NumberExpression</c> represents concreate expression.</summary>
+    public class NumberExpression : IExpression
+    {
+        private string name;
+        public NumberExpression(string name)
+        {
+            this.name = name;
+        }
+        public int Interpret(Context ctx)
+        {
+            return ctx.GetVariable(name);
+        }
+    }
 
-/// <summary>Class <c>Handler</c> represents abstraction.</summary>
+    /// <summary>Class <c>AddExpression</c> represents concreate expression.</summary>
+    public class AddExpression : IExpression
+    {
+        private IExpression left;
+        private IExpression right;
+        public AddExpression(IExpression left, IExpression right)
+        {
+            this.left = left;
+            this.right = right;
+        }
+        public int Interpret(Context ctx)
+        {
+            return left.Interpret(ctx) + right.Interpret(ctx);
+        }
+    }
+
+    /// <summary>Class <c>SubstractExpression</c> represents concreate expression.</summary>
+    public class SubstractExpression : IExpression
+    {
+        private IExpression left;
+        private IExpression right;
+        public SubstractExpression(IExpression left, IExpression right)
+        {
+            this.left = left;
+            this.right = right;
+        }
+        public int Interpret(Context ctx)
+        {
+            return left.Interpret(ctx) - right.Interpret(ctx);
+        }
+    }
+}
